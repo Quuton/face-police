23de0114fefefb1a2207131cde63d86cca8693f6
David Matthew Antonio <i_davidmatthew.antonio@stratpoint.com>
2024-10-11 03:51:53 +0800

    More stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import cv2
 from ultralytics import YOLO
-model = YOLO(f'{config.OUTPUT_DIR}best.pt')
+model = YOLO('output/best.pt')
 
 def get_prediction_frames(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
